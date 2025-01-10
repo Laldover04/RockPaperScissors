e13abc87963186962ea5ee043e93286fb3af86d0
Luke Aldover <122496790+Laldover04@users.noreply.github.com>
2025-01-09 22:21:52 -0700

    Created GUI
    Reworked rock paper scissors to use java GUI

--- GUI.java
+++ GUI.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,40 +9,82 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class GUI implements ActionListener {
-    int count = 0;
+    private int count = 0;
+    private int windowWidth = 600;
+    private int windowHeight = 300;
+
     private JFrame frame;
     private JLabel label;
     private JPanel panel;
+
     private JButton rockButton;
     private JButton paperButton;
-    private JButton scissorsButton;
+    private JButton scissorButton;
+    private JLabel passwordLabel;
+    private JLabel matchResult;
+    private JLabel arnoldLabel;
+    private JLabel score;
+    private JPasswordField passwordText;
+
+    JTextField userText;
 
     public GUI() {
-        frame = new JFrame();
-
-        rockButton = new JButton("Rock");
-        rockButton.addActionListener(this);
-        paperButton = new JButton("Paper");
-        paperButton.addActionListener(this);
-        scissorsButton = new JButton("Scissors");
-        scissorsButton.addActionListener(this);
-
-        label = new JLabel("Number of clicks: 0");
-
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(rockButton);
-        panel.add(paperButton);
-        panel.add(scissorsButton);
+        frame = new JFrame();
+        frame.setSize(windowWidth, windowHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        label = new JLabel("Rock Paper Scissors!");
+        label.setBounds(windowWidth / 3, 0, 200, 25);
         panel.add(label);
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
-        frame.pack();
+        // userText = new JTextField(20);
+        // userText.setBounds(100, 30, 165, 25);
+        // panel.add(userText);
+
+        // passwordLabel = new JLabel("Password");
+        // passwordLabel.setBounds(10, 60, 80, 25);
+        // panel.add(passwordLabel);
+
+        // passwordText = new JPasswordField();
+        // passwordText.setBounds(100, 60, 165, 25);
+        // panel.add(passwordText);
+
+        rockButton = new JButton("rock");
+        rockButton.setBounds(windowWidth / 2 - 155, 100, 90, 25);
+        rockButton.addActionListener(this);
+        panel.add(rockButton);
+
+        paperButton = new JButton("paper");
+        paperButton.setBounds(windowWidth / 2 - 55, 100, 90, 25);
+        paperButton.addActionListener(this);
+        panel.add(paperButton);
+
+        scissorButton = new JButton("scissors");
+        scissorButton.setBounds(windowWidth / 2 + 45, 100, 90, 25);
+        scissorButton.addActionListener(this);
+        panel.add(scissorButton);
+
+        matchResult = new JLabel("Good Luck!");
+        matchResult.setBounds(windowWidth / 2 - 110, 160, 300, 25);
+        panel.add(matchResult);
+
+        arnoldLabel = new JLabel("Arnold: ");
+        arnoldLabel.setBounds(windowWidth / 2 - 185, 160, 80, 25);
+        arnoldLabel.setFont(new Font("serif", Font.BOLD, 18));
+        panel.add(arnoldLabel);
+
+        score = new JLabel("score 0 - 0");
+        score.setBounds(windowWidth / 2 - 40, 20, 100, 25);
+        panel.add(score);
+
         frame.setVisible(true);
 
     }
@@ -49,8 +92,19 @@ public class GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        // Computer decides to cheat
+        if (e.getSource() == rockButton) {
+            matchResult.setText("I play paper! You Lose!");
+        } else if (e.getSource() == paperButton) {
+            matchResult.setText("I play scissors! You Lose!");
+        } else if (e.getSource() == scissorButton) {
+            matchResult.setText("I play rock! You Lose!");
+        }
         count++;
-        label.setText("number of clicks:" + count);
+        score.setText("score 0 - " + count);
     }
 
+    public static void main(String[] args) {
+        new GUI();
+    }
 };
